663f80115d92a6228db8d51a68985844d6a1e859
unknown <jubayerahmedtanvi@gmail.com>
2023-10-25 17:38:16 +0600

    have connected the server side to the client side

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9ca3bxa.mongodb.net/?retryWrites=true&w=majority `;
-console.log(uri);
 
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,12 +27,20 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+
+    app.post('/products', async(req,res) => {
+        const newProduct = req.body;
+        console.log(newProduct);
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
